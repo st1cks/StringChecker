a937a6dce2f297fda6d0fb7ae73767653ff916e4
BT_1N3_06 <rzhao3818@bths.edu>
2018-11-21 14:34:09 -0500

    done

--- src/StringCheckerRunner.java
+++ src/StringCheckerRunner.java
@@ -1,6 +1,17 @@
 public class StringCheckerRunner {
-    public class void main(String[] args){}
-    StringChecker sc1 = new CodeWordChecker(5,8,""$"");
-    StringChecker sc2 = new CodeWordChecker("pass");
+    public static void main(String[] args){
 
+        StringChecker sc1 = new CodeWordChecker(5,8,"$");
+        StringChecker sc2 = new CodeWordChecker("pass");
+
+        System.out.println(sc1.isValid("happy"));
+        System.out.println(sc1.isValid("happy$"));
+        System.out.println(sc1.isValid("Code"));
+        System.out.println(sc1.isValid("happyCode"));
+
+        System.out.println(sc2.isValid("MyPass"));
+        System.out.println(sc2.isValid("MypassPort"));
+        System.out.println(sc2.isValid("happy"));
+        System.out.println(sc2.isValid("1,000,000,000,000,000,000,000,000"));
+    }
 }
